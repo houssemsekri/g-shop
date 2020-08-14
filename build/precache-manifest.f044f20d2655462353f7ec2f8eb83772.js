0b1915ecd2f6e65b503a90c35b2473675d929ab3
houssemsekri64 <61097885+houssemsekri64@users.noreply.github.com>
2020-08-14 20:32:58 +0100

    make it better

---
+++ build/precache-manifest.f044f20d2655462353f7ec2f8eb83772.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbfe16395d5d0f6946e8aecc8abd199a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79922363860f126908a4",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "9d32479bec9e7bb68bd2",
+    "url": "/static/css/main.ce7d3cea.chunk.css"
+  },
+  {
+    "revision": "79922363860f126908a4",
+    "url": "/static/js/2.b5950533.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b5950533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d32479bec9e7bb68bd2",
+    "url": "/static/js/main.9db05c3d.chunk.js"
+  },
+  {
+    "revision": "32903e393911b9036b04",
+    "url": "/static/js/runtime-main.2dd8c1cc.js"
+  }
+]);
